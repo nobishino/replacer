2e3c06fc6633337d32feb830dc85de41370494a6
nobishino <nobishii11@gmail.com>
2024-08-13 17:29:48 +0900

    naive implementation

--- writer.go
+++ writer.go
@@ -3,18 +3,74 @@ package replacer
 import "io"
 
 type Writer struct {
-	underlying io.Writer
-	buf        []byte
+	underlying io.Writer // the Writer to which replaced byte stream is written
+	buf        []byte    // internal buffer for matching
+	filled     int       // buf[x] == 0 の時にそれが書き込みされていないのか0が書き込まれたのかを区別するためのindex
+	old        []byte
+	new        []byte
 }
 
 func (w *Writer) Write(p []byte) (n int, err error) {
-	return w.underlying.Write([]byte("bar"))
+	for _, b := range p {
+		if err := w.write(b); err != nil {
+			return n, err
+		}
+		n++
+	}
+	return n, nil
 }
 
-func NewWriter(w io.Writer, old, new string) *Writer {
+// 次に書き込みをするべきbufのindex = 書き込みが完了したidxの次のindex
+func (w *Writer) next() int {
+	return w.filled % len(w.buf)
+}
+
+func (w *Writer) write(b byte) error {
+	w.buf[w.filled%len(w.buf)] = b
+	w.filled++
+	if w.filled < len(w.buf) {
+		return nil
+	}
+	if w.match() {
+		w.filled = 0
+		if _, err := w.underlying.Write(w.new); err != nil {
+			return err
+		}
+	} else {
+		if _, err := w.underlying.Write(w.buf[:1]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// 最後に書き込まれたbyteを末尾とする部分列がoldと一致するかどうかを返す
+func (w *Writer) match() bool {
+	if w.filled < len(w.buf) {
+		return false
+	}
+	for i := 0; i < len(w.buf); i++ {
+		ii := (w.head() + i) % len(w.buf)
+		if w.buf[ii] != w.old[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// oldとのマッチングを開始するべきindexを返す
+func (w *Writer) head() int {
+	return w.next()
+}
+
+func NewWriter(w io.Writer, old, new []byte) *Writer {
 	buf := make([]byte, len(old))
 	return &Writer{
 		underlying: w,
 		buf:        buf,
+		old:        []byte(old),
+		new:        []byte(new),
 	}
 }
+
+var _ io.Writer = &Writer{}
--- writer_test.go
+++ writer_test.go
@@ -7,26 +7,27 @@ import (
 	"github.com/nobishino/replacer"
 )
 
-func TestWriter(t *testing.T) {
-	buf := new(bytes.Buffer)
-	w := replacer.NewWriter(buf, "foo", "bar")
-	w.Write([]byte("foo"))
-	if buf.String() != "bar" {
-		t.Errorf("got %q, want %q", buf.String(), "bar")
-	}
-}
+// func TestWriter(t *testing.T) {
+// 	buf := new(bytes.Buffer)
+// 	w := replacer.NewWriter(buf, "foo", "bar")
+// 	w.Write([]byte("foo"))
+// 	if buf.String() != "bar" {
+// 		t.Errorf("got %q, want %q", buf.String(), "bar")
+// 	}
+// }
 
 var tests = []struct {
 	old, new, in, out string
 }{
 	{"foo", "bar", "foo", "bar"},
 	{"foo", "bar", "ffoo", "fbar"},
+	// {"foo", "bar", "ffoofooo", "fbarbaro"},
 }
 
-func TestWriter2(t *testing.T) {
+func TestWriter(t *testing.T) {
 	for _, tt := range tests {
 		buf := new(bytes.Buffer)
-		w := replacer.NewWriter(buf, tt.old, tt.new)
+		w := replacer.NewWriter(buf, []byte(tt.old), []byte(tt.new))
 		_, err := w.Write([]byte(tt.in))
 		if err != nil {
 			t.Errorf("unexpected error: %v", err)
