e073b25e07ce2aa1c0f35d7ac5816d8c93139f57
nobishii <43101415+nobishino@users.noreply.github.com>
2024-08-14 17:16:00 +0900

    Implement replacer.Writer (though it is WIP) (#1)

---
+++ writer.go
@@ -0,0 +1,96 @@
+package replacer
+
+import (
+	"fmt"
+	"io"
+)
+
+type Writer struct {
+	underlying io.Writer // the Writer to which replaced byte stream is written
+	buf        []byte    // internal ring buffer for matching
+	filled     int       // number of bytes filled in buf.
+	old        []byte    // byte sequence to be replaced
+	new        []byte    // byte sequence to replace
+	n          int
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+	for _, b := range p {
+		if err := w.write(b); err != nil {
+			return n, err
+		}
+		n++
+	}
+	return n, nil
+}
+
+// 次に書き込みをするべきbufのindex = 書き込みが完了したidxの次のindex
+func (w *Writer) next() int {
+	return w.filled % len(w.buf)
+}
+
+func (w *Writer) shouldReplace() bool {
+	return w.n != 0
+}
+
+func (w *Writer) write(b byte) error {
+	w.buf[w.next()] = b
+	w.filled++
+	if w.filled < len(w.buf) { // まだマッチするかどうか判断できない
+		return nil
+	}
+	if w.match() && w.shouldReplace() {
+		w.filled = 0
+		w.n--
+		if _, err := w.underlying.Write(w.new); err != nil {
+			return err
+		}
+	} else {
+		if _, err := w.underlying.Write(w.buf[:1]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (w *Writer) Flush() error {
+	if w.filled < len(w.buf) {
+		if _, err := w.underlying.Write(w.buf[:w.filled]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// 最後に書き込まれたbyteを末尾とする部分列がoldと一致するかどうかを返す
+// w.filled < len(w.buf)の場合は呼び出してはいけない(panicする)
+func (w *Writer) match() bool {
+	if w.filled < len(w.buf) {
+		panic(fmt.Sprintf("match should not be called when filled < len(buf). but filled = %d, len(buf) = %d", w.filled, len(w.buf)))
+	}
+	for i := 0; i < len(w.buf); i++ {
+		ii := (w.head() + i) % len(w.buf)
+		if w.buf[ii] != w.old[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// oldとのマッチングを開始するべきindexを返す
+func (w *Writer) head() int {
+	return w.next()
+}
+
+func NewWriter(w io.Writer, old, new []byte, n int) *Writer {
+	buf := make([]byte, len(old))
+	return &Writer{
+		underlying: w,
+		buf:        buf,
+		old:        []byte(old),
+		new:        []byte(new),
+		n:          n,
+	}
+}
+
+var _ io.Writer = &Writer{}
---
+++ writer_test.go
@@ -0,0 +1,136 @@
+package replacer_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/nobishino/replacer"
+)
+
+var tests = []struct {
+	old, new, in, out string
+}{
+	{"foo", "bar", "foo", "bar"},
+	{"foo", "bar", "ffoo", "fbar"},
+	{"foo", "bar", "ffoofooo", "fbarbaro"},
+	{"f", "b", "ffoo", "bboo"},
+}
+
+func TestWriter(t *testing.T) {
+	for _, tt := range tests {
+		buf := new(bytes.Buffer)
+		w := replacer.NewWriter(buf, []byte(tt.old), []byte(tt.new), -1)
+
+		if _, err := w.Write([]byte(tt.in)); err != nil {
+			t.Errorf("unexpected error on Write: %v", err)
+		}
+		if err := w.Flush(); err != nil {
+			t.Fatalf("unexpected error on Flush: %v", err)
+		}
+		if buf.String() != tt.out {
+			t.Errorf("got %q, want %q", buf.String(), tt.out)
+		}
+	}
+}
+
+func TestWriterIsEquivalentToBytesReplace(t *testing.T) {
+	type test struct {
+		in  string
+		old string
+		new string
+		n   int
+	}
+	tests := []test{
+		{"hello", "l", "L", 0},
+		{"hello", "l", "L", -1},
+		{"hello", "x", "X", -1},
+		// {"", "x", "X", -1},
+		// {"radar", "r", "<r>", -1},
+		// {"", "", "<>", -1},
+		// {"banana", "a", "<>", -1},
+		// {"banana", "a", "<>", 1},
+		// {"banana", "a", "<>", 1000},
+		// {"banana", "an", "<>", -1},
+		// {"banana", "ana", "<>", -1},
+		// {"banana", "", "<>", -1},
+		// {"banana", "", "<>", 10},
+		// {"banana", "", "<>", 6},
+		// {"banana", "", "<>", 5},
+		// {"banana", "", "<>", 1},
+		// {"banana", "a", "a", -1},
+		// {"banana", "a", "a", 1},
+		// {"☺☻☹", "", "<>", -1},
+	}
+	title := func(tc test) string {
+		return fmt.Sprintf("(%q,%q,%q,%d)", tc.in, tc.old, tc.new, tc.n)
+	}
+	for _, tc := range tests {
+		t.Run(title(tc), func(t *testing.T) {
+			expect := bytes.Replace([]byte(tc.in), []byte(tc.old), []byte(tc.new), tc.n)
+
+			got := new(bytes.Buffer)
+			rw := replacer.NewWriter(got, []byte(tc.old), []byte(tc.new), tc.n)
+			if _, err := io.Copy(rw, strings.NewReader(tc.in)); err != nil {
+				t.Fatal(err)
+			}
+			if err := rw.Flush(); err != nil {
+				t.Fatal(err)
+			}
+
+			if !bytes.Equal(got.Bytes(), expect) {
+				t.Errorf("got %q, want %q", got.Bytes(), expect)
+			}
+		})
+	}
+}
+
+// copied from https://github.com/golang/go/blob/72735094660a475a69050b7368c56b25346f5406/src/bytes/bytes_test.go#L1601
+type ReplaceTest struct {
+	in       string
+	old, new string
+	n        int
+	out      string
+}
+
+var ReplaceTests = []ReplaceTest{
+	// {"hello", "l", "L", 0, "hello"},
+	// {"hello", "l", "L", -1, "heLLo"},
+	// {"hello", "x", "X", -1, "hello"},
+	// {"", "x", "X", -1, ""},
+	// {"radar", "r", "<r>", -1, "<r>ada<r>"},
+	// {"", "", "<>", -1, "<>"},
+	// {"banana", "a", "<>", -1, "b<>n<>n<>"},
+	// {"banana", "a", "<>", 1, "b<>nana"},
+	// {"banana", "a", "<>", 1000, "b<>n<>n<>"},
+	// {"banana", "an", "<>", -1, "b<><>a"},
+	// {"banana", "ana", "<>", -1, "b<>na"},
+	// {"banana", "", "<>", -1, "<>b<>a<>n<>a<>n<>a<>"},
+	// {"banana", "", "<>", 10, "<>b<>a<>n<>a<>n<>a<>"},
+	// {"banana", "", "<>", 6, "<>b<>a<>n<>a<>n<>a"},
+	// {"banana", "", "<>", 5, "<>b<>a<>n<>a<>na"},
+	// {"banana", "", "<>", 1, "<>banana"},
+	// {"banana", "a", "a", -1, "banana"},
+	// {"banana", "a", "a", 1, "banana"},
+	// {"☺☻☹", "", "<>", -1, "<>☺<>☻<>☹<>"},
+}
+
+func TestReplace(t *testing.T) {
+	for _, tt := range ReplaceTests {
+		in := append([]byte(tt.in), "<spare>"...)
+		in = in[:len(tt.in)]
+		buf := new(bytes.Buffer)
+		w := replacer.NewWriter(buf, []byte(tt.old), []byte(tt.new), tt.n)
+		if _, err := w.Write(in); err != nil {
+			t.Fatalf("unexpected error on Write: %v", err)
+		}
+		if err := w.Flush(); err != nil {
+			t.Fatalf("unexpected error on Flush: %v", err)
+		}
+		if s := buf.String(); s != tt.out {
+			t.Errorf("Replace(%q, %q, %q, %d) = %q, want %q", tt.in, tt.old, tt.new, tt.n, s, tt.out)
+		}
+	}
+}
